e744d50e5027f222e52affdb7acb2b9460f83944
Nikolai Krot <talpus@gmail.com>
2013-08-12 21:11:35 +0300

    Empty (initial) project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dicet::Application.config.secret_key_base = '1bcfda0529413f4407ec7a14b1735f49230a0eb9084bdc6f0afdd12618fad08ed071e664d294940930d13bb05779f5acb127dd3a7c95ef41749ab0430b7f09af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dicet::Application.config.session_store :cookie_store, key: '_dicet_session'
