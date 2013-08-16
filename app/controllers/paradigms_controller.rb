3763471496ffa59e67139c0b9925b2c4ae2a57de
Nikolai Krot <talpus@gmail.com>
2013-08-16 12:58:30 +0300

    words are updated when paradigm is saved

--- app/controllers/paradigms_controller.rb
+++ app/controllers/paradigms_controller.rb
@@ -17,20 +17,9 @@ class ParadigmsController < ApplicationController
 
   def create
     # bring a new paradigm and save it to db
+    save_paradigms params
+
     @paradigm = Paradigm.new
-    puts params
-
-    # extract individual paradigms from the form
-    each_paradigm(params) do |pdg_type, status, comment, words|
-      puts "WORDS:\n #{words.inspect}"
-      @pdg = Paradigm.new(paradigm_type: pdg_type, status: status, comment: comment)
-      @pdg.words.concat words
-      puts "WORDS:\n #{words.inspect}"
-#     @pdg.words.save
-      @pdg.save
-      words.map {|w| w.paradigm_id = @pdg.id } 
-    end
-
     render 'new'
   end
 
@@ -45,25 +34,39 @@ class ParadigmsController < ApplicationController
 
   private
 
-# [
-#  <Word id: nil, text: "book",    typo: nil, comment: nil, created_at: nil, updated_at: nil, tag: "VB", paradigm_id: nil>,
-#  <Word id: nil, text: "booking", typo: nil, comment: nil, created_at: nil, updated_at: nil, tag: "VBG", paradigm_id: nil>
-# ]
+  def save_paradigms params
+#    puts params
+    # extract individual paradigms from the form
+    each_paradigm(params) do |pdg_type, status, comment, words|
+      pdg = Paradigm.new(paradigm_type: pdg_type, status: status, comment: comment)
+      pdg.words.concat words
+#      puts "WORDS:\n #{words.inspect}"
+      pdg.save
+      words.map {|w| w.update_attributes(paradigm_id: pdg.id) } 
+    end
+  end
 
-  def each_paradigm(params)
+  def each_paradigm params
     params[:pdg].each do |pdg_type, data|
       words = []
       data.each do |tag, hash|
-        if tag !~ /^(status|comment)$/
+        if tag !~ /^(status|comment)$/ && !hash[:word].empty?
           # attributes that identify a Word that was not taken to a paradigm
           attrs = {text: hash[:word], tag: nil, paradigm_id: nil}
-          _word = Word.find_by(attrs)
-          puts "Found? #{_word.to_s}"
-          words << Word.where(attrs).first_or_create
-          puts __method__
+
+#          _word = Word.find_by(attrs)
+#          puts "Found? #{_word.to_s}"
+
+          # TODO: the user can change word.text as well
+          w = Word.where(attrs).first_or_create(text: hash[:word])
+          w.update_attributes(tag: hash[:tag])
+
+          words << w
         end
       end
-      yield pdg_type, data[:status], data[:comment], words
+      unless words.empty?
+        yield pdg_type, data[:status], data[:comment], words
+      end
     end
   end
 
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,6 @@
 class Word < ActiveRecord::Base
   belongs_to :paradigm
-  validates :text, presence: true, length: { maximum: 400 },
-                   uniqueness: true
+  validates :text, presence: true, length: { maximum: 400 }
+  # uniqueness: true -- word can legally repeat several times if
+  #  it has different tags and/or belongs to several paradigms
 end
