a56775db21cd674cb1b344712971b51f5e40776a
Nikolai Krot <talpus@gmail.com>
2013-08-21 22:57:27 +0300

    invalid files removed

--- db/20130815123518_add_paradigm_type_ref_to_paradigms.rb
+++
@@ -1,6 +0,0 @@
-class AddParadigmTypeRefToParadigms < ActiveRecord::Migration
-  def change
-    #add_column :paradigms, :paradigm_type, :string
-    add_reference :paradigms, :paradigm_type, index: true
-  end
-end
--- db/20130815123518_add_paradigm_type_to_paradigms.rb
+++
@@ -1,5 +0,0 @@
-class AddParadigmTypeToParadigms < ActiveRecord::Migration
-  def change
-    add_column :paradigms, :paradigm_type, :string
-  end
-end
