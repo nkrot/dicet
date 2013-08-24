7b09043cbef47544f67283b24a89783bd2315d0a
Nikolai Krot <talpus@gmail.com>
2013-08-24 20:47:00 +0300

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ end
 
 ######################################################################
 
-tagset = ["!", ["FO", "Formula"], ["FW", "Foreign word"], "(", ")", "LQ", "RQ", "DA", ",", ".", "...", ":", ";", "?", "ABL", "ABN", "ABX", "AP", "APS", "AT", "ATI", "BE", "BED", "BEDZ", "BEG", "BEM", "BEN", "BER", "BEZ", "CC", "CD", "CD-CD", "CD1", "CD1S", "CDS", "CS", "DO", "DOD", "DOZ", "DT", "DTI", "DTS", "DTX", "EX", "HV", "HVD", "HVG", "HVN", "HVZ", "IN", "JJ", "JJR", "JJT", "JNP", "MD", ["NN", "Noun common singular"], ["NNS", "Noun common plural"], "NNP", "NNPS",  ["NNU", "Unit of measurement, singular"], ["NNUS", "Unit of measurement, plural"], "NP", "NPS", "NPL", "NPLS", "NPT", "NPTS", "NR", "NRS", "OD", "PN", "PP$", "PP$$", "PP1A", "PP1AS", "PP1O", "PP1OS", "PP2", "PP3", "PP3A", "PP3AS", "PP3O", "PP3OS", "PPL", "PPLS", "QL", "QLP", "RB", "RBR", "RBT", "RI", "RN", "RP", "TO", "UH", "VB", "VBZ", "VBG", "VBD", "VBN", "WDT", "WP", "WRB", ["XNOT", "Negation particle"], "ZZ", "POS", "JJing", "JJed", "NOTAG"]
+tagset = ["!", ["FO", "Formula"], ["FW", "Foreign word"], "(", ")", "LQ", "RQ", "DA", ",", ".", "...", ":", ";", "?", "ABL", "ABN", "ABX", "AP", "APS", "AT", "ATI", "BE", "BED", "BEDZ", "BEG", "BEM", "BEN", "BER", "BEZ", "CC", "CD", "CD-CD", "CD1", "CD1S", "CDS", "CS", "DO", "DOD", "DOZ", "DT", "DTI", "DTS", "DTX", "EX", "HV", "HVD", "HVG", "HVN", "HVZ", "IN", "JJ", "JJR", "JJT", "JNP", "MD", ["NN", "Noun common singular"], ["NNS", "Noun common plural"], "NNP", "NNPS",  ["NNU", "Unit of measurement, singular"], ["NNUS", "Unit of measurement, plural"], "NP", "NPS", "NPL", ["NPLS", "Noun proper locative (or may be not) plural"], "NPT", "NPTS", "NR", "NRS", "OD", "PN", "PP$", "PP$$", "PP1A", "PP1AS", "PP1O", "PP1OS", "PP2", "PP3", "PP3A", "PP3AS", "PP3O", "PP3OS", "PPL", "PPLS", "QL", "QLP", "RB", "RBR", "RBT", "RI", "RN", "RP", "TO", "UH", "VB", "VBZ", "VBG", "VBD", "VBN", "WDT", "WP", "WRB", ["XNOT", "Negation particle"], "ZZ", "POS", "JJing", "JJed", "NOTAG"]
 
 tagset.each do |tag| 
   if tag.instance_of? Array
@@ -35,7 +35,8 @@ pdg_types = {
   "npt" => ["NPT", "NPTS"],
   "npl" => ["NPL", "NPLS"],
   "vb"  => ["VB", "VBZ", "VBG", "VBD", "VBN", "JJing", "JJed"],
-  "jj"  => ["JJ", "JJR", "JJT"]
+  "jj"  => ["JJ", "JJR", "JJT", "RB"],
+  "rb"  => ["RB", "RBR", "RBT"]
 }
 
 pdg_types.each do |name, tags|
@@ -69,13 +70,13 @@ tasks = \
  ["apples", "raining", "rains"],
  [0, "windy", "winds", "wall", "notes"],
  [0, "chair", "cherry", "advancedly", "advantageously", "omg"],
- [0, "success", "successful", "successfully"],
- [0, "net", "networks", "netto"],
- [0, "coin", "coins", "koen"],
- [0, "dwarf", "dwell", "dweller", "double"],
+ [0, "success", "successful", "successfully", "order"],
+ [0, "net", "networks", "netto", "dwells"],
+ [0, "coin", "coins", "koen", "mosquitoes"],
+ [0, "dwarf", "dwell", "dweller", "double", "keys"],
  [0, "enhance", "enhancement", "envisage", "enormous", "enormously", "eloquent"],
- [0, "five", "fake", "fool", "foment", "foster", "fluffy"],
- [0, "giant", "grotesque", "gothic", "goblin"],
+ [0, "five", "fake", "fool", "foment", "foster", "fluffy", "lorries"],
+ [0, "giant", "grotesque", "gothic", "goblin", "loosing"],
  [0, "kernel", "kompressor", "Kirk", "key", "keyboard"],
  [0, "lousy", "looser", "loose", "lorry"],
  [0, "masterpiece", "moisture", "mosquito"],
