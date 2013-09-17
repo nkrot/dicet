452f6f0fe2b3b5d30d4a6b2f4fba871c336dd8c9
Nikolai Krot <talpus@gmail.com>
2013-09-17 23:33:57 +0300

    updated task list is re-rendered from inside take.js.haml; will_paginate fixed to stay on the correct page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,12 +19,20 @@ class TasksController < ApplicationController
   end
 
   def take
+#    puts "PARAMS: #{params.inspect}"
+
     @task = Task.find(params[:id])
     @task.user = current_user
     @task.save
 
-    @tasks = Task.where(user_id: nil) # all unassigned tasks
-    @user_tasks = @task.user.tasks
+#    @tasks = Task.where(user_id: nil) # all unassigned tasks
+#    @user_tasks = @task.user.tasks
+
+    # TODO: same as in #index
+    @tasks = Task.where(user_id: nil).paginate(page:     params[:page],
+                                               per_page: TASKS_PER_PAGE,
+                                               order:    'priority DESC')
+    @user_tasks = Task.where(user_id: current_user)
   end
 
   def drop
