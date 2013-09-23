4c8564d844d9dd073449d75cb832d766d0de4e6a
nkrot <talpus@gmail.com>
2013-09-23 16:44:12 +0300

    paradigms that have dumped=true are visually different, in #edit and in task lists

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -2,7 +2,12 @@ module WordsHelper
 
   def links_to_paradigm_edit word
     word.homonyms.map do |w|
-      link_to "#{w.tagname}:#{w.paradigm_id}", edit_paradigm_path(:id => w.paradigm_id, :word_id => word.id)
+      html_options = {'class' => ['lnk_to_pdg']}
+      if w.paradigm.dumped?
+        html_options['class'] << 'dumped'
+      end
+
+      link_to "#{w.tagname}:#{w.paradigm_id}", edit_paradigm_path(:id => w.paradigm_id, :word_id => word.id), html_options
     end.join(", ")
   end
 end
