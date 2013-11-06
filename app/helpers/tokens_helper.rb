07454028ed8599462c3dcee7e788d4cbc84b3c4d
nkrot <talpus@gmail.com>
2013-11-06 15:58:52 +0300

    added highlighting of the column by which the table is sorted

--- app/helpers/tokens_helper.rb
+++ app/helpers/tokens_helper.rb
@@ -4,4 +4,11 @@ module TokensHelper
     params[name] == '1'
   end
 
+  def hi_if_ordered_by(colname)
+    if @order_by_column.to_s == colname.to_s
+      'orderby'
+    else
+      ''
+    end
+  end
 end
